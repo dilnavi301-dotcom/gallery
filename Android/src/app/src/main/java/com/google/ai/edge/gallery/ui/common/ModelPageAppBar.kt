8ce3670effd1692bd19f100b21facef5a87cf3e1
Yishuang Pang <ypang@google.com>
2026-05-05 09:15:05 -0700

    Set speculative decoding based on setting and model file ...
    PiperOrigin-RevId: 910728993

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/ui/common/ModelPageAppBar.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/ui/common/ModelPageAppBar.kt
@@ -216,6 +216,21 @@ fun ModelPageAppBar(
     if (!task.allowCapability(ModelCapability.LLM_THINKING, model)) {
       modelConfigs.removeIf { it.key == ConfigKeys.ENABLE_THINKING }
     }
+    var supportsSpeculativeDecoding = false
+    // Check if the model file supports speculative decoding.
+    try {
+      com.google.ai.edge.litertlm.Capabilities(model.getPath(context)).use {
+        supportsSpeculativeDecoding = it.hasSpeculativeDecodingSupport()
+      }
+    } catch (e: Exception) {
+      // Ignore exceptions and assume not supported.
+    }
+    if (
+      !supportsSpeculativeDecoding ||
+        !task.allowCapability(ModelCapability.SPECULATIVE_DECODING, model)
+    ) {
+      modelConfigs.removeIf { it.key == ConfigKeys.ENABLE_SPECULATIVE_DECODING }
+    }
     ConfigDialog(
       title = "Configurations",
       configs = modelConfigs,
